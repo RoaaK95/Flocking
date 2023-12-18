66bf8e9db5454696d4443ee6e8afedbca6d346f1
RoaaK95 <ruaakhuldoon.26@gmail.com>
2023-12-18 23:02:03 +0300

    second commit
    -Setting up the scene
    -swimming behavior

---
+++ Assets/Scripts/FlockManager.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlockManager : MonoBehaviour
+{
+    public static FlockManager FM;
+    public GameObject fishPrefab;
+    public int fishNum = 20;
+    public GameObject[] allFish;
+    public Vector3 swimLimits = new Vector3(5, 5, 5);
+
+    [Header("Speed Settings")]
+    [Range(0.0f, 5.0f)]
+    public float minSpeed;
+    [Range(0.0f, 5.0f)]
+    public float maxSpeed;
+
+    void Start()
+    {
+        allFish = new GameObject[fishNum];
+        for (int i = 0; i < fishNum; i++)
+        {
+
+
+            Vector3 pos = transform.position + new Vector3(Random.Range(-swimLimits.x, swimLimits.x)
+                                                         , Random.Range(-swimLimits.y, swimLimits.y)
+                                                         , Random.Range(-swimLimits.z, swimLimits.z));
+
+            allFish[i] = Instantiate(fishPrefab, pos, Quaternion.identity);
+        }
+        FM = this;
+    }
+
+     
+    void Update()
+    {
+
+    }
+}
